b051977b9342e35dc23600f3019b1a348bc58ce5
Romain Billard <romain.billard@hetic.net>
2018-06-20 22:02:51 +0200

    Improve responsive styles

--- src/components/ManagerSection/styles.js
+++ src/components/ManagerSection/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { media } from '../../theme'
 import { StyledText } from '../Text/styles'
 
 export const MainWrapper = styled.div`
@@ -9,6 +10,13 @@ export const TitleWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: ${({ center }) => (center ? 'center' : 'flex-start')};
+  text-align: ${({ center }) => (center ? 'center' : 'left')};
+  ${({ center }) =>
+    !center &&
+    `
+    max-width: 280px;
+    ${media.tablet`max-width: 320px;`};
+  `};
 
   ${StyledText} {
     margin-bottom: ${({ theme }) => theme.sizes.regular};
@@ -16,14 +24,31 @@ export const TitleWrapper = styled.div`
 `
 
 export const DashboardWrapper = styled.div`
+  position: relative;
+  overflow: hidden;
   display: flex;
   justify-content: space-between;
+  min-height: 300px;
+  ${media.tablet`min-height: 450px;`};
+  ${media.large`min-height: 500px;`};
 `
 
-export const DashboardIlluWrapper = styled.div``
+export const DashboardIlluWrapper = styled.div`
+  position: absolute;
+  right: -1002px;
+  ${media.tablet`right: -960px;`};
+  ${media.desktop`right: -500px;`};
+  ${media.large`right: -300px;`};
+  @media screen and (min-width: 1400px) {
+    right: -200px;
+  }
+  top: 0;
+`
 
 export const DashboardTitlesWrapper = styled.div`
-  margin-top: 12em;
-  margin-left: 6em;
+  margin-top: 6em;
+  ${media.desktop`margin-top: 12em`};
+  margin-left: 2em;
+  ${media.tablet`margin-left: 6em;`};
   margin-right: 2em;
 `

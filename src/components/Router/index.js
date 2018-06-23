c7d992d29a284059fdfd197401ffbbd87534fe14
Tom Bonnike <bonniketom@gmail.com>
2018-06-23 11:57:04 +0200

    Scroll to top on language change

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,16 +1,28 @@
-import React, { Fragment } from 'react'
-import { Switch, Route } from 'react-router-dom'
+// @flow
+import React, { Component, Fragment, type Children } from 'react'
+import { Switch, Route, withRouter } from 'react-router-dom'
 import Home from '../Home'
 import Pricing from '../Pricing'
 
-const Router = props =>
-  props.children(
-    <Fragment>
-      <Switch>
-        <Route path="/:locale/pricing" component={Pricing} />
-        <Route path="/:locale?" component={Home} />
-      </Switch>
-    </Fragment>
-  )
+type Props = { children: Children, location: any }
 
-export default Router
+class Router extends Component<Props> {
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0)
+    }
+  }
+
+  render() {
+    return this.props.children(
+      <Fragment>
+        <Switch>
+          <Route path="/:locale/pricing" component={Pricing} />
+          <Route path="/:locale?" component={Home} />
+        </Switch>
+      </Fragment>
+    )
+  }
+}
+
+export default withRouter(Router)
